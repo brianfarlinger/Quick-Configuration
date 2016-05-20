f4c2c85eae869cf47514f7a17953a18ac9c3e9e9
Brian Farlinger <brian@lilac.red>
2016-05-20 16:36:38 -0700

    Update kvm-centos-install.sh

--- kvm-centos-install.sh
+++ kvm-centos-install.sh
@@ -13,5 +13,5 @@ virt-install \
 --disk pool=libvirt-images,size=8 \
 --network bridge=virbr0,mac=RANDOM \
 --nographics \
---extra-args "http://cobbler.lilac.red/ks.cfg ksdevice=eth0 ip=dhcp console=tty0 console=ttyS0,115200" \
+--extra-args "http://cobbler.lilac.red/ks.cfg ksdevice=eth0 console=tty0 console=ttyS0,115200" \
 --debug
