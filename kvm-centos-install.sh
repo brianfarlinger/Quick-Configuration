4a79be3a2b8c6ee698e24a8d8ede27bfc377faae
Brian Farlinger <brian@lilac.red>
2016-05-20 15:50:36 -0700

    Update kvm-centos-install.sh

--- kvm-centos-install.sh
+++ kvm-centos-install.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 echo -n "Hostname: "
 read hostname
-echo $hostname > /tmp/hostname
+echo -n "Ram: "
+read RAM 
 virt-install \
 -n $hostname \
--r 1024 --vcpus=2 \
+-r $RAM --vcpus=2 \
 --cpu host \
---cdrom=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1511.iso \
+#--cdrom=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1511.iso \
+--location http://cobbler.lilac.red/CentOS-7-x86_64-Minimal-1511.iso \
 --os-variant=rhel7 \
 --disk pool=libvirt-images,size=8 \
 --network bridge=virbr0,mac=RANDOM \
 --nographics \
--x "ks=/var/lib/libvirt/scripts/ks.cfg"
+--extra-args "http://cobbler.lilac.red/ks.cfg console=tty0 console=ttyS0,115200" \
+--debug
